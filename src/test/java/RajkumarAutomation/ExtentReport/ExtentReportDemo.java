ae1c51c512bf4b125dee59a4c8b1897c1c239423
Rajkumar <rkr15250328@gmail.com>
2023-08-01 12:16:27 +0530

    IST architect

--- src/test/java/RajkumarAutomation/ExtentReport/ExtentReportDemo.java
+++ src/test/java/RajkumarAutomation/ExtentReport/ExtentReportDemo.java
@@ -21,6 +21,34 @@ public class ExtentReportDemo {
 		System.out.println(driver.getTitle());
 		
 	}
+	@Test
+	public void iitialdemo3() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		
+	}
+	@Test
+	public void iitialdemo1() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		
+	}
 	
 
 }
